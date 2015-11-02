c59fd8a98121a2e34f37024ca6ecd66c18c952da
js-acevedo10 <js.acevedo10@uniandes.edu.co>
2015-11-02 17:01:34 -0500

    StringEscape
    Arreglados los caracteres especiales para html.

--- src/main/java/utilities/TwitterJSONParser.java
+++ src/main/java/utilities/TwitterJSONParser.java
@@ -1,10 +1,17 @@
 package utilities;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+
 import twitter4j.Status;
 
 public class TwitterJSONParser {
 	public static String parseTwitt(Status status) {
-		String respuesta = status.getText().toLowerCase();
+		String name = StringEscapeUtils.escapeHtml4(status.getUser().getName()).replaceAll("[^\\x20-\\x7e]", "");
+		String screen_name = StringEscapeUtils.escapeHtml4("@" + status.getUser().getScreenName()).replaceAll("[^\\x20-\\x7e]", "");
+		String text = StringEscapeUtils.escapeHtml4(status.getText()).replaceAll("[^\\x20-\\x7e]", "");
+		
+		String respuesta = "<h1>" + name + "</h1><h2>" + "" + screen_name + "</h2><p>" + text + "</p>";
+		
 		return respuesta;
 	}
 }
